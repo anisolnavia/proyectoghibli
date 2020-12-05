3d30af732d47e70b3f7cb0deec2e4ffb6efad166
Anisol Navia <anisolnaviaz@gmail.com>
2020-12-05 20:01:21 -0300

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Films } from '../model/films';
+import { FilmService } from '../services/film.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  films:Films[];
+  film:Films[];
+  constructor(private filmsghibli:FilmService) { }
+ 
+  ngOnInit(): void {
+    this.filmsghibli.getFilms().subscribe(peliculas=>this.films=peliculas);
+  }
+ /*mostrarPelicula(id){
+ this.filmsghibli.getPelicula(id).subscribe(pelicula=>this.film=pelicula);
+ console.log(this.film)
+ }*/
+}
